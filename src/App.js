e1ebc7fdc60a39a6e5e1e3ffa431e794f022892e
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-02-17 11:16:04 -0500

    moving header into it's own component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react'
 
 import './App.css';
 
-import logo from './Images/Wisteria-logo.png';
-import Home from './Components/Home/home'
+import Header from './Components/Header/header';
+import Home from './Components/Home/home';
 
 
 
 function App() {
   return(
     <div className="Body">
-        <div className='Nav'>
-          <img className='logo' src={logo}></img>  
-        </div>
-
+        
+        <Header></Header>
         <Home></Home>
         <div className='Shop'>
           <h2>This is the shop</h2>
