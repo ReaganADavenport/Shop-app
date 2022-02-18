b31ca7cd97780c9590bff903584d045d8da3504c
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-02-18 13:48:08 -0500

    added basic code to card js&css

---
+++ src/Components/Card/card.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './card.css';
+
+
+const Card = () => {
+    return(
+        <div className="Body">
+          
+        </div>
+    )
+}
+
+export default Card;
