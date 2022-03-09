8508d754d6122ca4ac3f5433882b558afb80c5ad
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-03-09 15:09:12 -0500

    added a third cad for lotus to see if that would mess up the grid (it did), rounded the corners of the images on the cards

--- src/Components/Store-front/shop.js
+++ src/Components/Store-front/shop.js
@@ -5,6 +5,7 @@ import Card from '../Card/card';
 
 import Balloon from '../../Images/Balloon-flowers.png';
 import Pansy from '../../Images/Pansies.jpg';
+import Lotus from '../../Images/Lotus.jpg';
 
 const Shop = () => {
     return(
@@ -13,6 +14,7 @@ const Shop = () => {
         <div className='Shelf'>
           <Card itemImage={Balloon} itemName={'Balloon Flowers'} itemPrice={'$15.99'}></Card>
           <Card itemImage={Pansy} itemName={'Pansies'} itemPrice={'$12.99'}></Card>
+          <Card itemImage={Lotus} itemName={'Lotus Flowers'} itemPrice={'$25.99'}></Card>
         </div>
         
       </div>
