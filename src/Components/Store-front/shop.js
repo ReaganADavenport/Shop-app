9dfc2522fb6dd2ad88cfff0612c8999322e16b43
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-03-09 14:55:12 -0500

    added another card (for pansies) and added a container for the items named Shelf and added css grid

--- src/Components/Store-front/shop.js
+++ src/Components/Store-front/shop.js
@@ -4,12 +4,17 @@ import './shop.css';
 import Card from '../Card/card';
 
 import Balloon from '../../Images/Balloon-flowers.png';
+import Pansy from '../../Images/Pansies.jpg';
 
 const Shop = () => {
     return(
         <div className='Shop'>
-        <h2>This is the shop</h2>
-        <Card itemImage={Balloon} itemName={'Balloon Flowers'} itemPrice={'$15.99'}></Card>
+        <h2>Shop</h2>
+        <div className='Shelf'>
+          <Card itemImage={Balloon} itemName={'Balloon Flowers'} itemPrice={'$15.99'}></Card>
+          <Card itemImage={Pansy} itemName={'Pansies'} itemPrice={'$12.99'}></Card>
+        </div>
+        
       </div>
     )
 }
