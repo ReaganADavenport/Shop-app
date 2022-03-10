433be2ece0c4fa14cad433e5c247152c7412399b
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-03-10 17:17:31 -0500

    making the text block in home smaller, cutting down the text in the text block

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -13,7 +13,7 @@ const Home = () => {
           <div className='Text'>
             <h1>Fujiwara Florists</h1>
             <p>Only the highest quality flowers</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           
           <img className='flower-left' src={flower_right}></img>
