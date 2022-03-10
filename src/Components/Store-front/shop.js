36b207481026188afcac9cb08f741603e46ca9a8
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-03-10 17:51:31 -0500

    changing the lotus image, deleting stucco image, adding peony and snap dragon cards, and making the home component smaller

--- src/Components/Store-front/shop.js
+++ src/Components/Store-front/shop.js
@@ -5,7 +5,9 @@ import Card from '../Card/card';
 
 import Balloon from '../../Images/Balloon-flowers.png';
 import Pansy from '../../Images/Pansies.jpg';
-import Lotus from '../../Images/Lotus.jpg';
+import Lotus from '../../Images/Lotus.png';
+import Peony from '../../Images/Peony.png';
+import Snap from '../../Images/Snap-dragon.png';
 
 const Shop = () => {
     return(
@@ -15,6 +17,8 @@ const Shop = () => {
           <Card itemImage={Balloon} itemName={'Balloon Flowers'} itemPrice={'$15.99'}></Card>
           <Card itemImage={Pansy} itemName={'Pansies'} itemPrice={'$12.99'}></Card>
           <Card itemImage={Lotus} itemName={'Lotus Flowers'} itemPrice={'$25.99'}></Card>
+          <Card itemImage={Peony} itemName={'Peonies'} itemPrice={'$14.99'}></Card>
+          <Card itemImage={Snap} itemName={'Snap-dragons'} itemPrice={'$13.99'}></Card>
         </div>
         
       </div>
