0ad48a70938d7f6ff2a806db9a069f32bae7cd2e
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-03-17 14:22:03 -0400

    added cart png to header, added flex, justified content to space-between

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import './header.css'
 
 import logo from '../../Images/Wisteria-logo.png';
+import cart from '../../Images/shopping-cart.png';
 
 const Header = () => {
     return(
         <div className='Nav'>
-            <img className='logo' src={logo}></img>  
+            <img className='logo' src={logo}></img>
+            <img className='cart' src={cart}></img>
       </div>
     )
 };
